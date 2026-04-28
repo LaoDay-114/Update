a05ed6295dc4a86549c87763c89d89c0313de18c
LaoDay-114 <924403444@qq.com>
2026-04-28 17:09:49 +0800

    Main

--- PythonUse/__init__.py
+++ PythonUse/__init__.py
@@ -1,40 +1,45 @@
 import time
-import subprocess
 import requests
 import urllib3
+import subprocess
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
 def StartUpdateSession(CheckTime: int = 5, Version: str = None):
-    # 转换定时检查
     print(f"PythonUse {Version}")
     check_seconds = CheckTime * 60
-    repo_username = "LaoDay-114"
-    repo_name = "Update"
+
     while True:
-        # 检查更新
-        print("[1/4] Check Update")
+        print("[1/5] Check Update")
         try:
-            version_url = f"https://raw.githubusercontent.com/{repo_username}/{repo_name}/Updatepack/lastversion.txt"
-            resp = requests.get(version_url, timeout=10, verify=False)
-            if resp.status_code == 200:
-                last_version = resp.text.strip()
-                print(f"[2/4] Latest Version: {last_version}")
-                if Version != last_version:
-                    print("[3/4] Update Available.")
-                    download_url = f"https://raw.githubusercontent.com/{repo_username}/{repo_name}/Updatepack/{last_version}/{last_version}.updatepack"
-                    print(f"[4/4] Download Update: {download_url}")
-                    subprocess.run([
-                        "gettool.exe", download_url
-                    ], shell=True)
-                else:
-                    print("[3/4] No Update.")
+            resp = requests.get(
+                "https://raw.githubusercontent.com/LaoDay-114/Update/Updatepack/lastversion.txt",
+                verify=False,
+                timeout=10
+            )
+            last_version = resp.text.strip()
+            print(f"[2/5] Latest Version: {last_version}")
+
+            if Version != last_version:
+                print("[3/5] Update Available.")
+                download_url = f"https://raw.githubusercontent.com/LaoDay-114/Update/Updatepack/{last_version}/last.updatepack"
+                print(f"[4/5] Downloading...")
+
+                file_content = requests.get(download_url, verify=False, timeout=20).content
+                with open(f"{last_version}.updatepack", "wb") as f:
+                    f.write(file_content)
+
+                print("Download Success!")
+                print("[5/5] Update")
+                cmd = f"update.exe -old last.updatepack"
+                print(cmd)
+                subprocess.run(cmd, shell=True)
+
             else:
-                print("[2/4] Failed to get latest version")
+                print("[3/5] No Update.")
+
         except Exception as e:
-            print(f"[1/4] Error: {e}")
-        # 等待指定时间
-        print(f"Wait for {check_seconds} seconds")
+            print("[2/5] Check failed, skip")
+
+        print(f"Wait {check_seconds}s\n")
         time.sleep(check_seconds)
-       
